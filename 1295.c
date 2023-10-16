14683ac8e9741104f292928d56ced78c62643581
h811044 <142477229+h811044@users.noreply.github.com>
2023-10-16 15:15:48 +0800

    1295. Find Numbers with Even Number of Digits

---
+++ 1295.c
@@ -0,0 +1,23 @@
+// 1295. Find Numbers with Even Number of Digits
+
+// Helper function to check if the number of digits is even
+bool hasEvenDigits(int num) {
+    int digitCount = 0;
+    while (num) {
+        digitCount++;
+        num /= 10;
+    }
+    return (digitCount & 1) == 0;
+}
+
+int findNumbers(int* nums, int numsSize) {
+    // Counter to count the number of even digit integers
+    int evenDigitCount = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (hasEvenDigits(nums[i]))
+            evenDigitCount++;
+    }
+
+    return evenDigitCount;
+};
