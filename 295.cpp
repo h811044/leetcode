da81635aa37999356c985a2b0dfe43d5c941d613
h811044 <142477229+h811044@users.noreply.github.com>
2023-12-22 00:27:20 +0800

    295. Find Median from Data Stream

---
+++ 295.cpp
@@ -0,0 +1,33 @@
+// 295. Find Median from Data Stream
+
+class MedianFinder {
+private:
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
+public:
+    MedianFinder() {
+    }
+
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        return maxHeap.size() > minHeap.size() ? maxHeap.top() : ((double)maxHeap.top() + minHeap.top()) * 0.5;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
+ 
